0123b604780de1e6951523b6219ca6d602720577
Pinyaev Vladimir <avallakhh@gmail.com>
2024-07-02 19:08:43 +0300

    changes

---
+++ bin/games/brain-calc.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import QuestionandGreeting from "../src/cli.js";
+import { game } from "../src/index.js";
+
+QuestionandGreeting();
+
+const descriptionGame = `What is the result of the expression?`;
+
+const gameRound = (minNum1, maxNum2, operators) => {
+  minNum1 = Math.floor(Math.random() * 100);
+  maxNum2 = Math.ceil(Math.random() * 100);
+  operators = [`-`, `+`, `*`];
+
+  const operator = operators[Math.floor(Math.random() * operators.length)];
+  const question = `Question: ${minNum1} ${operator} ${maxNum2}`;
+  let correctAnswer;
+  switch (question) {
+    case "-":
+      correctAnswer = minNum1 - maxNum2;
+      break;
+    case "+":
+      correctAnswer = minNum1 + maxNum2;
+      break;
+    case "*":
+      correctAnswer = minNum1 * maxNum2;
+      break;
+  }
+  return [question, correctAnswer.toString()];
+};
+
+game(descriptionGame, gameRound);
--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -2,9 +2,9 @@
 
 import readlineSync from "readline-sync";
 
-console.log("Welcome to the Brain Games!");
+console.log(`Welcome to the Brain Games!`);
 
-const userName = readlineSync.question("May I have your name? ");
+const userName = readlineSync.question(`May I have your name? `);
 console.log(`Hello, ${userName}!`);
 
 function evenOrOdd() {
@@ -16,16 +16,14 @@ function evenOrOdd() {
     return number % 2 === 0;
   }
 
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
   for (let i = 0; i < 3; i += 1) {
     const getRandomNumber = randomNumber(10, 80);
-    console.log("Question: " + `${getRandomNumber}`);
-    const answerUser = readlineSync.question("Your answer: ");
+    console.log(`Question: ${getRandomNumber}`);
+    const answerUser = readlineSync.question(`Your answer: `);
 
-    const correctAnswer = isEven(getRandomNumber) ? "yes" : "no";
+    const correctAnswer = isEven(getRandomNumber) ? `yes` : `no`;
     if (correctAnswer === answerUser.toLowerCase()) {
-      console.log("Correct!");
+      console.log(`Correct!`);
     } else {
       console.log(
         `${answerUser} is wrong answer :(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`
