66cb9319cd645712f8603ea30fc9d401b2832e64
Vladimir Pinyev <Avallakhh@gmail.com>
2024-07-22 17:36:10 +0300

    edited games 6

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
-export const getRandomInRange = (min = 0, max = 100) => Math.floor(min + Math.random() * (max - min + 1));
+export const getRandomInRange = (min = 0, max = 100) => 
+  (Math.floor(min + Math.random() * (max - min + 1)));
 
 export const getRandomOperator = () => {
   const operators = ['-', '+', '*'];
