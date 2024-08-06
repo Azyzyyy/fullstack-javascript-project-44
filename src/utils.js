151b8b5b3a00ac829c2cae4819c31c654c13d59e
Pinyaev Vladimir <avallakhh@gmail.com>
2024-08-07 00:21:42 +0300

    Edited games 13

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
 export const roundsCount = 3;
 
-export const getRandomInRange = (min = 0, max = 100) => {
-  return Math.floor(min + Math.random() * (max - min + 1));
-}
+export const getRandomInRange = (min = 0, max = 100) => 
+  Math.floor(min + Math.random() * (max - min + 1));
